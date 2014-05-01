4e4fbb91d04d6ce97865d45c0f6a9818330652fc
barrymillham <j.mart.x93@gmail.com>
2014-04-30 20:16:01 -0400

    Working

--- Ray Generation/Ray Generation/main.cpp
+++ Ray Generation/Ray Generation/main.cpp
@@ -22,8 +22,8 @@ int main(int argc, char** argv) {
 	vec3 C = M - eye;
 	vec3 UP = vec3(0,1,0);
 	vec3 V = UP;
-	V *= glm::tan(45.0f);
-	mat4 rotationMatrix = glm::rotate(mat4(1.0f), 90.0f, V);
+	V *= glm::tan(glm::radians(45.0f));
+	mat4 rotationMatrix = glm::rotate(mat4(1.0f), 90.0f, C);
 	vec4 H4 = vec4(V.x, V.y, V.z, 0.0f) * rotationMatrix;
 	H4 *= 1.333f;
 	vec3 H = vec3(H4.x, H4.y, H4.z);
@@ -58,25 +58,22 @@ int main(int argc, char** argv) {
 	
 	*/
 
-
-	//Yeah this doesn't work. Worth a shot. 
 	for (int x = 0; x < width; x++) {
 		for (int y = 0; y < height; y++) {
 			vec3 rayPositionH = H;
 			vec3 rayPositionV = V;
-			rayPositionH *= (2*x/(width-1)-1);
-			rayPositionV *= (2*y/(height-1)-1);
+			rayPositionH *= (2 * (double)x) / (width - 1) - 1;
+			rayPositionV *= (2 * (double)y) / (height - 1) - 1;
 			vec3 P = M + rayPositionH + rayPositionV;
 			
 			//D = (P-E)/|P-E|
 			vec3 D = normalize(P - eye);
-			output(x,y)->Red = D.x;
-			output(x,y)->Green = D.y;
-			output(x,y)->Blue = D.z;
+			output(x,y)->Red = glm::abs(D.x*255);
+			output(x,y)->Green = glm::abs(D.y*255);
+			output(x,y)->Blue = glm::abs(D.z*255);
 		}
 	}
-	
-	
+
 	output.WriteToFile("output.bmp");
 	return 0;
 }
